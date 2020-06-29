b1dab266c0bb8898ba03f85e2bc4cf0b659c24b0
Robert_Yang <robert_yang@htc.com>
2020-06-29 17:56:42 +0800

    96

--- 96_Unique_BST.cpp
+++ 96_Unique_BST.cpp
@@ -1,17 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-void FindBST(vector<bool> &vec, int output)
+int FindBST(int start, int end)
 {
+  if (start >= end)
+    return 1;
+
+  int ans = 0;
+  // 不同的tree node當root
+  for (int i = start; i <= end; i++)
+  {
+    int leftTreeNum = FindBST(start, i - 1);
+    int rightTreeNum = FindBST(i + 1, end);
+    ans += leftTreeNum * rightTreeNum;
+  }
+  return ans;
 }
 int numTrees(int n)
 {
-  vector<bool> record;
-  record.reserve(n);
-  vec.reserve(n);
-  for (int i = 0; i < n; i++)
-  {
-    record.push_back(false);
-  }
-  int output = 0;
-  FindBST(record, output);
+  if (n == 0)
+    return 0;
+  return FindBST(1, n);
 }
