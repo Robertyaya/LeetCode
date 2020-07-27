12d64496248d8c3a1bf5f11abbf03eeb1a2519af
Robert_Yang <robert_yang@htc.com>
2020-07-27 22:08:30 +0800

    127 ongoning

---
+++ 73.set-matrix-zeroes.cpp
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode id=73 lang=cpp
+ *
+ * [73] Set Matrix Zeroes
+ *
+ * https://leetcode.com/problems/set-matrix-zeroes/description/
+ *
+ * algorithms
+ * Medium (42.95%)
+ * Likes:    2185
+ * Dislikes: 305
+ * Total Accepted:    323.3K
+ * Total Submissions: 751K
+ * Testcase Example:  '[[1,1,1],[1,0,1],[1,1,1]]'
+ *
+ * Given a m x n matrix, if an element is 0, set its entire row and column to
+ * 0. Do it in-place.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 
+ * [
+ * [1,1,1],
+ * [1,0,1],
+ * [1,1,1]
+ * ]
+ * Output: 
+ * [
+ * [1,0,1],
+ * [0,0,0],
+ * [1,0,1]
+ * ]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 
+ * [
+ * [0,1,2,0],
+ * [3,4,5,2],
+ * [1,3,1,5]
+ * ]
+ * Output: 
+ * [
+ * [0,0,0,0],
+ * [0,4,5,0],
+ * [0,3,1,0]
+ * ]
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * A straight forward solution using O(mn) space is probably a bad idea.
+ * A simple improvement uses O(m + n) space, but still not the best
+ * solution.
+ * Could you devise a constant space solution?
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    void FillZero(vector<vector<int>> &matrix, int row, int col)
+    {
+        for (int i = 0; i < matrix[0].size(); i++)
+            matrix[row][i] = 0;
+
+        for (int i = 0; i < matrix.size(); i++)
+            matrix[i][col] = 0;
+    }
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        vector<vector<int>> clone = matrix;
+        for (int i = 0; i < clone.size(); i++)
+        {
+            for (int j = 0; j < clone[0].size(); j++)
+            {
+                if (clone[i][j] == 0)
+                    FillZero(matrix, i, j);
+            }
+        }
+    }
+};
+// @lc code=end
